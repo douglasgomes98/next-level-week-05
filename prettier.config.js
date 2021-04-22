2c0264ad7df4f5ece902da4a8c2379273d30caa8
Douglas Gomes <douglasgomes.rr@gmail.com>
2021-04-22 16:50:41 -0400

    chore: add eslint and prettier

---
+++ prettier.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  semi: true,
+  singleQuote: true,
+  arrowParens: 'avoid',
+  trailingComma: 'all',
+  endOfLine: 'auto',
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+};
